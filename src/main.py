f8ddccee16abc2f836c44ef9964da890f74a6a8d
CatMoons <sheogor2@gmail.com>
2024-12-17 20:48:02 +0300

    create main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
+from reports import (load_transactions, rep_category_spending, rep_spend_on_working_or_weekends,
+                     rep_spending_on_weekdays)
 from services import analyze_cashback, extract_phone_numbers, get_transactions_with_phones
-from reports import load_transactions, rep_category_spending, rep_spending_on_weekdays, rep_spend_on_working_or_weekends
-from utils import transactions_xlsx, web_search_xcl, num_card_account
+from utils import num_card_account, transactions_xlsx, web_search_xcl
 from views import get_greeting, parse_datetime, process_excel_data
 
 file_path = "E:/pycharm_project/transaction_analysis_web/data/operations.xlsx"
@@ -24,12 +25,13 @@ def main():
     print(get_greeting(date_now))
     print(process_excel_data(file_path, date_input))
     print(web_search_xcl(file_open_xlsx, input_search))
-    print(num_card_account(file_open_xlsx,input_card))
+    print(num_card_account(file_open_xlsx, input_card))
     print(analyze_cashback(file_path, year, month))
     print(get_transactions_with_phones(file_path))
     print(rep_category_spending(load_transaction, input_search, date_input))
     print(rep_spending_on_weekdays(load_transaction, date_input))
     print(rep_spend_on_working_or_weekends(load_transaction, date_input))
+
 
 if __name__ == "__main__":
     main()
